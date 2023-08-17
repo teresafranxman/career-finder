5077c8563c70ec81b53090186e2d332e9718e8d5
Teresa <teresafranxman97@gmail.com>
2023-08-16 21:13:58 -0500

    Created component for the individual job post

--- src/app/jobs/[id]/page.js
+++ src/app/jobs/[id]/page.js
@@ -1,4 +1,5 @@
 import { getJob } from "./getJob";
+import Link from "next/link";
 
 const Job = async ({ params }) => {
 	const job = await getJob(params);
@@ -9,6 +10,7 @@ const Job = async ({ params }) => {
 			<h2>{job.company_name}</h2>
 			<div dangerouslySetInnerHTML={{ __html: job.text }} />
 			<div>
+				<Link href={job.url}>apply now</Link>
 				<button>Save Job</button>
             </div>
         </main>
