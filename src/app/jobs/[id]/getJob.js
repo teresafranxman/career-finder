c161d73d27b100160f880c76c0687d604661929e
Teresa <teresafranxman97@gmail.com>
2023-10-05 15:32:28 -0500

    Installed Redux TK for state management, updated getJobs and getJob functions to catch errors.

--- src/app/jobs/[id]/getJob.js
+++ src/app/jobs/[id]/getJob.js
@@ -1,13 +1,16 @@
 import { headers } from "next/headers";
 
 export const getJob = async (params) => {
-	const { id } = params;
-	const headersInstance = headers();
-	const authorization = headersInstance.get("authorization");
-	const response = await fetch(`https://findwork.dev/api/jobs/${id}`, {
-		headers: { authorization: `Token ${process.env.KEY}` },
-	});
+	try {
+		const { id } = params;
+		const headersInstance = headers();
+		const authorization = headersInstance.get("authorization");
+		const response = await fetch(`https://findwork.dev/api/jobs/${id}`, {
+			headers: { authorization: `Token ${process.env.KEY}` },
+		});
 
-	return response.json();
+		return response.json();
+	} catch (error) {
+		console.log("Error: ", error);
+	}
 };
-
--- src/app/jobs/getJobs.js
+++ src/app/jobs/getJobs.js
@@ -1,11 +1,15 @@
 import { headers } from "next/headers";
 
 export const getJobs = async () => {
-	const headersInstance = headers();
-	const authorization = headersInstance.get("authorization");
-	const response = await fetch("https://findwork.dev/api/jobs/", {
-		headers: { authorization: `Token ${process.env.KEY}` },
-	});
+	try {
+		const headersInstance = headers();
+		const authorization = headersInstance.get("authorization");
+		const response = await fetch("https://findwork.dev/api/jobs/", {
+			headers: { authorization: `Token ${process.env.KEY}` },
+		});
 
-	return response.json();
+		return response.json();
+	} catch (error) {
+		console.log("Error: ", error);
+	}
 };
