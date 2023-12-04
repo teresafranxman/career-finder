44d1e4d7a171f7a28b567f6d636c4f213afa3656
Teresa <teresafranxman97@gmail.com>
2023-12-04 14:52:16 -0600

    Updated cypress version to 13.6.0

--- src/app/StoreProvider.js
+++ src/app/StoreProvider.js
@@ -1,7 +1,12 @@
 "use client";
+import { useRef } from "react";
 import { Provider } from "react-redux";
 import { store } from "./lib/store";
 
 export const Providers = ({ children }) => {
+	const storeRef = useRef();
+
+	console.log(storeRef)
+
 	return <Provider store={store}>{children}</Provider>;
 };
