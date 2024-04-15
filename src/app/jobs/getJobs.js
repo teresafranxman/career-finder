22e8f33b35d7b7014962388e42b71fa4b138e97b
teresafranxman <teresafranxman97@gmail.com>
2024-04-15 15:59:00 -0500

    Updating getJobs to follow NextJS upgrade to app router guide.

--- src/app/jobs/getJobs.js
+++ src/app/jobs/getJobs.js
@@ -1,17 +1,13 @@
 import { headers } from "next/headers";
 
 export const getJobs = async () => {
-  try {
-    const headersInstance = headers();
-    const authorization = headersInstance.get("authorization");
-    const response = await fetch("https://findwork.dev/api/jobs/", {
-      headers: { authorization: `Token ${process.env.KEY}` },
-    });
+  const headersInstance = headers().get("authorization");
 
-    const jobs = response.json();
+  const response = await fetch("https://findwork.dev/api/jobs/", {
+    headers: { authorization: `Token ${process.env.KEY}` },
+  });
 
-    return jobs;
-  } catch (error) {
-    console.log("Error: ", error);
-  }
+  const jobs = await response.json();
+
+  return jobs;
 };
